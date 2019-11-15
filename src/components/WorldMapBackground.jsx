41f73c0a11ce80c5a5d226191312d67727d4d0a0
Duncan Gibbs <duncgibbs@gmail.com>
2019-11-15 00:05:06 -0600

    adds environment variable for API token

--- src/components/WorldMapBackground.jsx
+++ src/components/WorldMapBackground.jsx
@@ -10,7 +10,7 @@ export default class WorldMapBackground extends Component {
         return (
             <div id='world-map-div'>
                 <GoogleMapReact
-                    bootstrapURLKeys={{ key: '' }}
+                    bootstrapURLKeys={{ key: process.env.GOOGLE_MAPS_API_KEY }}
                     center={this.center || this.props.location}
                     zoom={this.props.zoom}
                     options={this.props.options}
